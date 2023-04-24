33b55c2dc2fbfab2e3910a68a7932d6b958499a6
ABRAHAM2J <103059744+ABRAHAM2J@users.noreply.github.com>
2023-04-24 10:03:31 -0400

    Add files via upload

---
+++ Pregunta1/inf/inf.php
@@ -0,0 +1,35 @@
+<?php
+    $nombres = array("Informática", "Matemática", "Biologia");
+    $links = array("./inf.php", "../mat/mat.php", "../bio/bio.php");
+    $images = array("../images/informatica.jpg", "../images/matematica.jpg", "../images/biologia.jpg");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pregunta 1</title>
+    <link rel="stylesheet" type="text/css" href="../estilos.css">
+</head>
+<body>
+    <img src="../images/info.jpeg">
+    <h2 class="main-title">Facultad de Ciencias Puras y Naturales</h2>
+    <h4 class="main-subtitle">Carrera de Informatica</h4>
+    <div class="menu">
+        <?php 
+            for ($i=0; $i < count($nombres); $i++) { 
+                echo "<div class='container'>";
+                echo    "<a href='$links[$i]' class='card'>";
+                echo        "<h3 class='title'>Carrera de $nombres[$i]</h3>";
+                echo        "<img src='$images[$i]' style='heigth: 230px; width: 90%;'>";
+                echo    "</a>";
+                echo "</div>";
+            }
+        ?>
+        <div class="btn">
+            <a href="../index.php">Regresar al menu</a>
+        </div>
+    </div>
+</body>
+</html>
